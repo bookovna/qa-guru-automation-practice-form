9a3fe7bb69d9624b6d9d51a1bc723e72c1ae202c
Анна Чепкасова <bukovna@yandex.ru>
2022-01-15 23:29:15 +0300

    adds attachments, video, etc

--- src/test/java/com/bookovna/tests/TestBase.java
+++ src/test/java/com/bookovna/tests/TestBase.java
@@ -3,11 +3,13 @@ package com.bookovna.tests;
 import com.bookovna.pages.RegistrationPage;
 import com.codeborne.selenide.Selenide;
 import com.codeborne.selenide.logevents.SelenideLogger;
+import helpers.Attach;
 import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 
 import com.codeborne.selenide.Configuration;
+import org.openqa.selenium.remote.DesiredCapabilities;
 
 public class TestBase {
 
@@ -17,10 +19,21 @@ public class TestBase {
     static void beforeAll() {
         SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
         Configuration.startMaximized = true;
+        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub/";
+
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("enableVNC", true);
+        capabilities.setCapability("enableVideo", true);
+
+        Configuration.browserCapabilities = capabilities;
     }
 
     @AfterEach
     void closeWebDriver() {
         Selenide.closeWebDriver();
+        Attach.screenshotAs("Last screenshot");
+        Attach.pageSource();
+        Attach.browserConsoleLogs();
+        Attach.addVideo();
     }
 }
