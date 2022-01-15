a09dc23f0f3d96baf07aee0168b77591042242e8
Анна Чепкасова <bukovna@yandex.ru>
2022-01-15 23:12:35 +0300

    adds selenide listener

--- src/test/java/com/bookovna/tests/TestBase.java
+++ src/test/java/com/bookovna/tests/TestBase.java
@@ -2,6 +2,8 @@ package com.bookovna.tests;
 
 import com.bookovna.pages.RegistrationPage;
 import com.codeborne.selenide.Selenide;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 
@@ -13,6 +15,7 @@ public class TestBase {
 
     @BeforeAll
     static void beforeAll() {
+        SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
         Configuration.startMaximized = true;
     }
 
