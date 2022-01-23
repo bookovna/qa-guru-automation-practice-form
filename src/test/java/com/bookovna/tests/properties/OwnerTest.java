ec2490eaf2eb85d24bc64c3558ca2dfb5a58fe3d
Анна Чепкасова <bukovna@yandex.ru>
2022-01-23 22:20:55 +0300

    edits build.gradle

--- src/test/java/com/bookovna/tests/properties/OwnerTest.java
+++ src/test/java/com/bookovna/tests/properties/OwnerTest.java
@@ -2,10 +2,12 @@ package com.bookovna.tests.properties;
 
 import com.bookovna.config.CredentialsConfig;
 import org.aeonbits.owner.ConfigFactory;
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
 import static java.lang.String.format;
 
+@Tag("Owner")
 public class OwnerTest {
     public CredentialsConfig credentials =
             ConfigFactory.create(CredentialsConfig.class);
