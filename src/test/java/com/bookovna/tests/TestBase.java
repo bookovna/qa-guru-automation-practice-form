452c2cc43c547d5399c708feab7a0ad668e6501c
Анна Чепкасова <bukovna@yandex.ru>
2022-01-28 22:25:09 +0300

    пробуем достать передать часть ссылки из дженкинса

--- src/test/java/com/bookovna/tests/TestBase.java
+++ src/test/java/com/bookovna/tests/TestBase.java
@@ -1,25 +1,33 @@
 package com.bookovna.tests;
 
+import com.bookovna.config.CredentialsConfig;
 import com.bookovna.pages.RegistrationPage;
+import com.bookovna.tests.properties.OwnerTest;
 import com.codeborne.selenide.Selenide;
 import com.codeborne.selenide.logevents.SelenideLogger;
 import com.bookovna.helpers.Attach;
 import io.qameta.allure.selenide.AllureSelenide;
+import org.aeonbits.owner.ConfigFactory;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 
 import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.Tag;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
+@Tag("Owner")
 public class TestBase {
 
     RegistrationPage registrationPage = new RegistrationPage();
 
     @BeforeAll
     static void beforeAll() {
+        CredentialsConfig credentials =
+                ConfigFactory.create(CredentialsConfig.class);
+
         SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
         Configuration.startMaximized = true;
-        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub/";
+        Configuration.remote = "https://" + credentials.login() + ":" + credentials.password() + "@selenoid.autotests.cloud/wd/hub/";
 
         DesiredCapabilities capabilities = new DesiredCapabilities();
         capabilities.setCapability("enableVNC", true);
